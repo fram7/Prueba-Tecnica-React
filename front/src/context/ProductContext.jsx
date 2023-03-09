b5d05bce6173ff0c10df1a4b9b64c436354fd1dd
fram7 <fram07@gmail.com>
2023-03-09 11:53:31 -0500

    Mejora en la explicacion de localstorage

--- front/src/context/ProductContext.jsx
+++ front/src/context/ProductContext.jsx
@@ -5,15 +5,26 @@ export const ProductContext = createContext();
 
 //Provider de datos y funciones
 const ProductProvider = ({ children }) => {
+  //1. Creamos una variable constante con KEY de Localstorage
   const _PRODUCT_LOCALSTORAGE = "lsProduct";
 
+  //2. Creamos una propiedad lsCarrito que trae lo almacenado en localstorage
   const lsCarrito = localStorage.getItem(_PRODUCT_LOCALSTORAGE);
 
+  //3.1 lsCarrito es null (signficia que no tengo nada almacenado en LS) => inicio un arreglo vacio
+  // 3.2 si lsCarrito trae algo (string) signifca que ya guarde algo y lo deserealizo para convertiro en el arreglo de items del carrito
   const [carrito, setCarrito] = useState(lsCarrito != null ? JSON.parse(lsCarrito) : []);
 
-  useEffect(() => {
-    localStorage.setItem(_PRODUCT_LOCALSTORAGE, JSON.stringify(carrito));
-  }, [carrito, setCarrito]);
+  //Opcion con USE EFFECT 4. Cree un useEffect que escucha a carrito y setCarrito y cada vez que carrito se modifica (setCarrito) guardo ese resultado en el localstorage. Como es LS paso el JSON (Arreglo) a un String utiliznado JSON.stringify
+  // useEffect(() => {
+  //   localStorage.setItem(_PRODUCT_LOCALSTORAGE, JSON.stringify(carrito));
+  // }, [carrito, setCarrito]);
+
+  ////Opcion SIN USE EFFECT (Mejor)  Funcion que setea el carrito y lo guarda en localstorage
+  const actualizarCartrito = (unCarrito) => {
+    setCarrito(unCarrito);
+    localStorage.setItem(_PRODUCT_LOCALSTORAGE, JSON.stringify(unCarrito));
+  };
 
   const addCarrito = (product) => {
     const copyCarrito = [...carrito];
@@ -28,7 +39,7 @@ const ProductProvider = ({ children }) => {
         product: product,
       });
     }
-    setCarrito(copyCarrito);
+    actualizarCartrito(copyCarrito);
   };
 
   const removeCarrito = (product) => {
@@ -43,7 +54,7 @@ const ProductProvider = ({ children }) => {
       if (productExistente.count === 0) {
         copyCarrito.splice(indexProductExistente, 1);
       }
-      setCarrito(copyCarrito);
+      actualizarCartrito(copyCarrito);
     }
   };
 
